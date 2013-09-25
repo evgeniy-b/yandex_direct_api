0a051d6cbf51d253a93ae51293bf80eb09ba9a62
Eugeniy Belyaev <eugeniy.b@garin-studio.ru>
2013-09-25 19:31:52 +0400

    exception & call fix

--- lib/yandex_direct/api.rb
+++ lib/yandex_direct/api.rb
@@ -4,6 +4,18 @@ require "active_support/core_ext"
 
 module Yandex
   module Direct
+    class ApiException < StandardError
+      attr_reader :message, :code, :details
+
+      def initialize(message, code, details)
+        @message, @code, @details = message, code, details
+      end
+
+      def to_s
+        "#{code}: #{message}"
+      end
+    end
+
     class Api
       attr_reader :token, :login, :app_id, :locale
 
@@ -11,28 +23,28 @@ module Yandex
         @token, @login, @app_id, @locale = token, login, app_id, locale
       end
 
-      def method_missing(method, *args)
-        request method.to_s.camelize, args
+      def method_missing(method, param = nil)
+        request method.to_s.camelize, param
       end
 
 
         private
 
-        def request_params(method, params)
+        def request_params(method, param)
           {
             application_id: app_id,
             login: login,
             token: token,
             locale: locale,
             method: method,
-            params: params
+            param: param
           }
         end
 
-        def request(method, params)
+        def request(method, param)
           request = Net::HTTP::Post.new "/json-api/v4/"
-          request.body = request_params(method, params).to_json
-            
+          request.body = request_params(method, param).to_json
+
           response = Net::HTTP.start("api.direct.yandex.ru", 443, use_ssl: true) do |http|
             http.verify_mode = OpenSSL::SSL::VERIFY_NONE
             http.request request
--- yandex_direct_api.gemspec
+++ yandex_direct_api.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'yandex_direct_api'
-  s.version     = '0.1.0'
+  s.version     = '0.1.1'
   s.date        = '2013-09-25'
   s.summary     = 'Yandex.Direct api wrapper'
   s.description = "Yandex.Direct api wrapper"
   s.authors     = ["Eugeniy Belyaev"]
   s.email       = 'eugeniy.b@garin-studio.ru'
-  s.files       = ['lib/yandex_direct/api.rb', 'lib/yandex_direct/api_exception.rb']
+  s.files       = ['lib/yandex_direct/api.rb']
   s.homepage    = 'https://github.com/zhekanax/yandex_direct_api'
 
   s.add_runtime_dependency 'activesupport', '>= 3.0.0'
